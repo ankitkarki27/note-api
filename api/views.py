56d1e7e11d876e0b7f7415414b5dc864195a4fc5
ankitkarki15 <ankitkarki8088@gmail.com>
2024-12-01 04:43:46 +0545

    code and files update

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from .models import Note
 from .serializers import NoteSerializer
 
+# make views here
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
